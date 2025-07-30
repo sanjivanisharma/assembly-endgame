8c838eaa8678409ff0f914019f17b0af72d97374
Sanjivani Sharma <sanjivanisharma01@gmail.com>
2025-07-30 14:44:57 +0530

    Design changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,28 @@
+import { languages } from "./languages"
+
 export default function App() {
 
+  const languageElements = languages.map((lang) => (
+    <span
+      style={{ backgroundColor: lang.backgroundColor, color: lang.color }}
+      className="chip"
+      key={lang.name}
+    >{lang.name}</span>
+  ))
+
   return (
-    <h1>Game here</h1>
+    <main>
+      <header>
+        <h1>Assembly: Endgame</h1>
+        <p>Guess the word in under 8 attempts to keep the programming world safe from Assembly!</p>
+      </header>
+      <section className="game-status">
+        <h2>You win!</h2>
+        <p>Well done!🎉</p>
+      </section>
+      <section className="language-chips">
+        {languageElements}
+      </section>
+    </main>
   )
 }
